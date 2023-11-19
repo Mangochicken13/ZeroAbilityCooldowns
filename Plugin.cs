5fedf6214c174e2cd52018c46d898e94abe10036
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2023-11-19 14:20:20 +0800

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BoplFixedMath;
+
+namespace ZeroAbilityCooldowns
+{
+    //[BepInPlugin(GUID, Name, Version)]
+    [BepInPlugin("Mangochicken.ZeroAbilityCooldowns", "ZeroAbilityCooldowns", "1.1.0")]
+    [BepInProcess("BoplBattle.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            On.Ability.GetCooldown += Ability_InstantCooldown;
+            On.InstantAbility.GetCooldown += InstantAbility_InstantCooldown;
+
+            //TODO: Work on making this a slider or at least some option in the settings
+
+            Logger.LogInfo($"Plugin \"ZeroAbilityCooldowns\" is loaded!");
+        }
+
+        private Fix Ability_InstantCooldown(On.Ability.orig_GetCooldown orig, Ability self)
+        {
+            return Fix.Zero;
+        }
+
+        private Fix InstantAbility_InstantCooldown(On.InstantAbility.orig_GetCooldown orig, InstantAbility self)
+        {
+            return self.IsRope ? orig.Invoke(self) : Fix.One / (Fix)6;
+            // Smoke is so fast that it absorbs all other input, a little bit of delay is ideal
+            // Unfortunately i don't know how to differentiate between smoke and everything else
+        }
+    }
+}
